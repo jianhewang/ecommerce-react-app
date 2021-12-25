a024cc0a3469710f62e0e9a22ae7e79fe1f03fd0
jianhewang <kuyasda@hotmail.com>
2021-12-25 13:23:23 -0500

    built components for homepage with styles

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MenuItem from "../../components/menu-items/menu-item.component";
+import Directory from "../../components/directory/directory.component";
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
+
+export default HomePage;
