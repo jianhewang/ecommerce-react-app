c1ff18958d077966ba9a321aa6e685911e6ce561
jianhewang <kuyasda@hotmail.com>
2021-12-27 20:36:14 -0500

    Build remove item function and create functionality to add and decrese check out item qut item quantity

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -14,3 +14,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     // qty property gets attached the first time around since this if block won't run when it's a new item
     return [...cartItems, {...cartItemToAdd, quantity: 1}]
 }
+
+export const removeFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    )
+
+    if (existingCartItem.quantity === 1){
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map(
+        cartItem => 
+        cartItem.id === cartItemToRemove.id ?
+        { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    )
+}
