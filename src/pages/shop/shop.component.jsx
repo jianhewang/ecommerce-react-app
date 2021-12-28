9b273060b457a5305203cb86c24d799609280430
jianhewang <kuyasda@hotmail.com>
2021-12-28 16:28:03 -0500

    Move directory and shop data into reducer and create selectors

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import { selectShopCollections } from "../../redux/shop/shop.selectors";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+// import SHOP_DATA from "./shop.data";
+// import { connect } from "react-redux";
+// import { createStructuredSelector } from "reselect";
+// import { selectShopCollections } from "../../redux/shop/shop.selectors";
+// import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-const ShopPage = ({collections}) => (
+import { Route } from "react-router-dom";
+
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from "../collection/collection.component";
+
+const ShopPage = ({ match }) => (
 
         <div className="shop-page">
-            {
-                collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))
-            }
+           <Route exact path={`${match.path}`} component={CollectionsOverview} />
+           <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
         </div>
     
 )
 
-const mapStateToProps = createStructuredSelector(
-    {collections: selectShopCollections}
-)
+// const mapStateToProps = createStructuredSelector(
+//     {collections: selectShopCollections}
+// )
 
-export default connect(mapStateToProps)(ShopPage);
+// export default connect(mapStateToProps)(ShopPage);
+
+export default ShopPage;
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,35 @@
 import { createSelector } from "reselect";
 
+// const COLLECTION_ID_MAP = {
+//     hats: 1,
+//     sneakers: 2,
+//     jackets: 3,
+//     womens: 4,
+//     mens: 5
+// }
+
 const selectShop = state => state.shop;
 
-export const selectShopCollections = createSelector(
+export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 )
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
+// find collection id matching the url parameter of our collection id map
+// export const selectCollection = collectionParam => 
+//     createSelector(
+//         [selectCollections],
+//         collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionParam])
+//     )
+
+// when we have a set of data as object not array, we can simplify the method
+export const selectCollection = collectionParam => 
+    createSelector(
+        [selectCollections],
+        collections => collections[collectionParam]
+    )
